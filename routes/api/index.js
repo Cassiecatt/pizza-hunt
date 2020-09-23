5d5b139e59693fec580030bc6929978c6c93f73c
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-09-22 21:09:20 -0700

    added front end functionality

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
-const router = require('express');
-const pizzaRoutes = reqyure('./pizza-routes.js');
+const router = require('express').Router();
+const pizzaRoutes = require('./pizza-routes');
 
-//add prefix of '/pizzas' to routes created in 'pizza-routes.js'
-router.request('/pizzas', pizzaRoutes);
+// add prefix of `/pizzas` to routes created in `pizza-routes.js`
+router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
